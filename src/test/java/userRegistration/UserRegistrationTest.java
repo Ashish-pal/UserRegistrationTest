3f13aa58aaa966fe439a60cf554d29402e5b5a59
Ashish Pal <ak987982@gmail.com>
2023-05-04 17:43:34 +0530

    Added user registration test and refactored code

--- src/test/java/userRegistration/UserRegistrationTest.java
+++ src/test/java/userRegistration/UserRegistrationTest.java
@@ -1,6 +1,38 @@
 package userRegistration;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
-public class UserRegistrationTest {
 
+public class UserRegistrationTest {
+    UserRegistrationMain validator = new UserRegistrationMain();
+
+    @Test
+    public void givenFirstName() {
+        boolean result = validator.checkFirstName();
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName() {
+        boolean result = validator.checkLastName();
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmailAddress() {
+        boolean result = validator.checkEmail();
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenMobile() {
+        boolean result = validator.checkMobileNumber();
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword() {
+        boolean result = validator.checkPassword();
+        Assert.assertEquals(true, result);
+    }
 }
